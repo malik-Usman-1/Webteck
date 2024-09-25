66805090f331e007ef7ad60afd8a74be025bf7ef
malik-Usman-1 <ujamshaid62@gmail.com>
2024-09-25 15:59:51 +0500

    add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Home1 from './Home/Home1';
+
+function App() {
+  return (
+   <>
+   <Home1/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Home/Home1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home1 = () => {
+  return (
+    <div>
+      Usman malik
+    </div>
+  )
+}
+
+export default Home1
